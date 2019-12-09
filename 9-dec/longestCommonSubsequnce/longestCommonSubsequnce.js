7dfb88f90af62583393f0d7a56d9f05fad5cfe1a
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-09 11:59:25 +0530

    dec-9 exercise

---
+++ 9-dec/longestCommonSubsequnce/longestCommonSubsequnce.js
@@ -0,0 +1,52 @@
+
+function longestCommonSubsequnce(stringA, stringB) {
+    const lcsMatrix = Array(stringB.length + 1).fill(null).map(() => Array(stringA.length + 1).fill(null));
+    for(let columnIndex = 0; columnIndex <= stringA.length; columnIndex+= 1) {
+        lcsMatrix[0][columnIndex] = 0;
+    }
+    for(let rowIndex = 0; rowIndex <= stringB.length; rowIndex+= 1) {
+      lcsMatrix[rowIndex][0] = 0;
+    }
+    for (let rowIndex = 1; rowIndex <= stringB.length; rowIndex += 1) {
+      for (let columnIndex = 1; columnIndex <= stringA.length; columnIndex += 1) {
+        if (stringA[columnIndex - 1] === stringB[rowIndex - 1]) {
+          lcsMatrix[rowIndex][columnIndex] = lcsMatrix[rowIndex - 1][columnIndex - 1] + 1;
+        } else {
+          lcsMatrix[rowIndex][columnIndex] = Math.max(
+            lcsMatrix[rowIndex - 1][columnIndex],
+            lcsMatrix[rowIndex][columnIndex - 1],
+          );
+        }
+      }
+    }
+    if(!lcsMatrix[stringB.length][stringA.length]) {
+      return [''];
+    }
+    const longestSequence = [];
+    let columnIndex = stringA.length;
+    let rowIndex = stringB.length;
+    while(columnIndex > 0 || rowIndex > 0) {
+              if(stringA[columnIndex - 1] === stringB[rowIndex - 1]) {
+                longestSequence.unshift(stringA[columnIndex - 1]);
+                columnIndex -= 1;
+                rowIndex -= 1;
+              } else if (lcsMatrix[rowIndex][columnIndex] === lcsMatrix[rowIndex][columnIndex - 1]) {
+                // Move left.
+                columnIndex -= 1;
+              } else {
+                // Move up.
+                rowIndex -= 1;
+              }
+    }
+    return longestSequence;
+
+  }
+  
+  export {
+    longestCommonSubsequnce,
+  };
+  
+  console.log(longestCommonSubsequnce(
+    ['A', 'B', 'C', 'D', 'G', 'H'],
+    ['A', 'E', 'D', 'F', 'H', 'R'],
+  ))
---
+++ 9-dec/truth-check/truthCheck.js
@@ -0,0 +1,9 @@
+
+function truthCheck(collection, predicate) {
+    return collection.every((item) => item[predicate] && Boolean(item[predicate]));
+  }
+  
+  export {
+    truthCheck,
+  };
+  

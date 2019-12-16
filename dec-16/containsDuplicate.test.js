4feaa49912956c44d340bb7674e830b05fe5a2be
Mohammad Wasi <mdwasi15@gmail.com>
2019-12-16 18:35:25 +0530

    correction

--- dec-16/containsDuplicate.test.js
+++ dec-16/containsDuplicate.test.js
@@ -9,7 +9,7 @@ describe('containsDuplicate', () => {
     const myArray = [1, 2, 3, 4];
     expect(containsDuplicate(myArray)).toBe(false);
   });
-  test('should return false', () => {
+  test('should return true', () => {
     const myArray = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
     expect(containsDuplicate(myArray)).toBe(true);
   });
